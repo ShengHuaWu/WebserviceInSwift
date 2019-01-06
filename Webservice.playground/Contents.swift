594cc8457fb16fe541ea8d4889f01c618c1dc676
ShengHua Wu <fantasy0404@gmail.com>
2019-01-06 17:56:01 +0100

    Update implementation.

--- Webservice.playground/Contents.swift
+++ Webservice.playground/Contents.swift
@@ -1,58 +1,97 @@
 import Foundation
 import PlaygroundSupport
 
-enum SerializationError: Error {
-    case missing(String)
+struct Person: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case name
+        case gender
+        case birth = "birth_year"
+    }
+    
+    let name: String
+    let gender: String
+    let birth: String
 }
 
-struct Acronym {
-    let id: Int
-    let short: String
-    let long: String
+struct PeopleResponse: Decodable {
+    let results: [Person]
 }
 
-typealias JSONDictionary = [String : Any]
+enum HTTPMethod<Parameters> {
+    case get(Parameters)
+    case post(Parameters)
+}
 
-extension Acronym {
-    init(json: JSONDictionary) throws {
-        guard let id = json["id"] as? Int else {
-            throw SerializationError.missing("id")
+extension HTTPMethod {
+    var name: String {
+        switch self {
+        case .get: return "GET"
+        case .post: return "POST"
         }
-        
-        guard let short = json["short"] as? String else {
-            throw SerializationError.missing("short")
+    }
+}
+
+extension HTTPMethod where Parameters: Encodable {
+    var body: Data? {
+        switch self {
+        case .get:
+            return nil
+        case .post(let parameters):
+            return try? JSONEncoder().encode(parameters)
         }
-        
-        guard let long = json["long"] as? String else {
-            throw SerializationError.missing("long")
-        }
-        
-        self.id = id
-        self.short = short
-        self.long = long
     }
 }
 
 struct Resource<Model> {
-    let url: URL
+    var urlRequest: URLRequest
     let parse: (Data) throws -> Model
 }
 
-extension Resource {
-    init(url: URL, parseJSON: @escaping (Any) throws -> Model) {
-        self.url = url
+extension Resource where Model: Decodable {
+    init<Parameters>(url: URL, method: HTTPMethod<Parameters>) where Parameters: Encodable {
+        self.urlRequest = URLRequest(url: url)
+        self.urlRequest.httpMethod = method.name
+        self.urlRequest.httpBody = method.body
         self.parse = { data in
-            let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-            return try parseJSON(json)
+            try JSONDecoder().decode(Model.self, from: data)
+        }
+    }
+    
+    init(get url: URL, method: HTTPMethod<[String: Any]>)  {
+        guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
+            preconditionFailure("Invalid url")
+        }
+        
+        switch method {
+        case .get(let parameters):
+            urlComponents.queryItems = parameters.map { key, value in
+                return URLQueryItem(name: key, value: "\(value)")
+            }
+        default: break
+        }
+        
+        self.urlRequest = URLRequest(url: urlComponents.url!)
+        self.urlRequest.httpMethod = method.name
+        self.parse = { data in
+            try JSONDecoder().decode(Model.self, from: data)
+        }
+    }
+    
+    init(url: URL, method: HTTPMethod<Void> = .get(())) {
+        self.urlRequest = URLRequest(url: url)
+        self.urlRequest.httpMethod = method.name
+        self.parse = { data in
+            try JSONDecoder().decode(Model.self, from: data)
         }
     }
 }
 
-extension Acronym {
-    static let all = Resource<[Acronym]>(url: URL(string: "http://localhost:8080/acronyms")!, parseJSON: { json in
-        guard let dictionaries = json as? [JSONDictionary] else { throw SerializationError.missing("acronyms") }
-        return try dictionaries.map(Acronym.init)
-    })
+//let m = HTTPMethod.get(["id": 109876, "name": "%(*!/"])
+//let r = Resource<PeopleResponse>(get: URL(string: "https://apple.com")!, method: m)
+//print(r.urlRequest.url?.absoluteString ?? "")
+
+extension PeopleResponse {
+    static let people = Resource<PeopleResponse>(url: URL(string: "https://swapi.co/api/people")!)
 }
 
 enum Result<Model> {
@@ -60,9 +99,11 @@ enum Result<Model> {
     case failure(Error)
 }
 
-final class Webservice {
+extension URLSession {
     func load<Model>(resource: Resource<Model>, completion: @escaping (Result<Model>) -> Void) {
-        URLSession.shared.dataTask(with: resource.url) { (data, _, error) in
+        dataTask(with: resource.urlRequest) { [weak self] (data, _, error) in
+            defer { self?.finishTasksAndInvalidate() }
+            
             if let error = error {
                 DispatchQueue.main.async {
                     completion(.failure(error))
@@ -85,8 +126,7 @@ final class Webservice {
     }
 }
 
-let webservice = Webservice()
-webservice.load(resource: Acronym.all) { result in
+URLSession.shared.load(resource: PeopleResponse.people) { result in
     debugPrint(result)
     PlaygroundPage.current.finishExecution()
 }
